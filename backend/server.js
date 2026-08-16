a1917b6c34d82361fcb322c043eb32fb126f353e
Adetunji Marvellous <adetunjimarvellous09@gmail.com>
2026-08-16 18:02:47 +0100

    Refactor code structure for improved readability and maintainability

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ process.on('uncaughtException', (err) => {
 });
 
 const DB = process.env.DB;
+console.log('db string', db);
 
 mongoose.connect(DB).then(() => {
   console.warn('DB connection successful ✅');
